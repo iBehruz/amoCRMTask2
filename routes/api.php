8570ddf26832cdc26887fdeb9b72164f67b7abf8
iBehruz <61056456+iBehruz@users.noreply.github.com>
2023-08-21 13:50:42 +0500

    исправление по feadback

--- routes/api.php
+++ routes/api.php
@@ -43,7 +43,99 @@ Route::post('/contact', function (Request $request) {
 
         // проверка на дубль
         $isDubl = false;
-        $url = 'https://bilhomov.amocrm.ru/api/v4/contacts';
+        $url = 'https://bilhomov.amocrm.ru/api/v4/contacts?with=leads';
+
+        $client = new GuzzleHttp\Client([
+            'verify' => false,
+        ]);
+
+        $response = $client->get(
+            $url,
+            [
+                'headers' => ['Authorization' => "Bearer ".$token, "Content-Type"=> "application/json"]
+            ]);
+
+        $body = json_decode($response->getBody()->getContents());
+        $dublContact = null;
+
+        if($body != null){
+            $contacts = $body->_embedded->contacts;
+            foreach ($contacts as $contact){
+                foreach ($contact->custom_fields_values as $field){
+                    if($field->field_code == "PHONE"){
+                        foreach ($field->values as $value){
+                           $isDubl = $data["phone"] == $value->value;
+                           if($isDubl)  break;
+                        }
+                    }
+                    if($isDubl) break;
+                }
+                if($isDubl) { $dublContact = $contact; break; };
+            }
+        }
+        //если есть дубль вернет warning
+        if($isDubl){
+
+            //проверям на наличие успешной сделки (142)
+            $isLead142 =false;
+            foreach ($dublContact->_embedded->leads as $lead){
+                $url = $lead->_links->self->href;
+
+                $client = new GuzzleHttp\Client([
+                    'verify' => false,
+                ]);
+                $response = $client->get(
+                    $url,
+                    [
+                        'headers' => ['Authorization' => "Bearer ".$token, "Content-Type"=> "application/json"]
+                    ]);
+
+                $bodyLead = json_decode($response->getBody()->getContents());
+
+                if($bodyLead->status_id == 142){
+                    $isLead142 = true;
+                    break;
+                }
+            }
+
+            //создаем покупателя если есть успешная сделка контакта
+            if($isLead142){
+                // создание покупателя
+                $bodyRequestCustomer = array(
+                    [
+                        "name"=> $dublContact->name,
+                        "_embedded"=> [
+                            "tags"=> array([
+                                "name"=> $dublContact->name
+                                ]
+                            )
+                        ]
+                    ]
+                );
+
+                $url2 = 'https://bilhomov.amocrm.ru/api/v4/customers';
+
+                $client2 = new GuzzleHttp\Client([
+                    'verify' => false,
+                ]);
+
+                $response2 = $client2->post(
+                    $url2,
+                    [
+                        'headers' => ['Authorization' => "Bearer ".$token, "Content-Type"=> "application/json"],
+                        'body' => json_encode($bodyRequestCustomer)
+                    ]
+                );
+
+                $body2 = $response2->getBody()->getContents();
+            }
+
+
+           return response(["success"=>false, "data"=>null, "message"=>"Warning dubl contact."],200);
+        }
+
+        // получение списка пользователей и выбрать рандомного ответственного пользователя
+        $url = 'https://bilhomov.amocrm.ru/api/v4/users';
 
         $client = new GuzzleHttp\Client([
             'verify' => false,
@@ -54,30 +146,18 @@ Route::post('/contact', function (Request $request) {
                 'headers' => ['Authorization' => "Bearer ".$token, "Content-Type"=> "application/json"]
             ]);
 
-        $body = json_decode($response->getBody()->getContents());
+        $bodyUsers = json_decode($response->getBody()->getContents());
 
-        if($body != null){
-            $contacts = $body->_embedded->contacts;
-            foreach ($contacts as $contact){
-                foreach ($contact->custom_fields_values as $field){
-                    if($field->field_code == "PHONE"){
-                        foreach ($field->values as $value){
-                           $isDubl = $data["phone"] == $value->value;
-                        }
-                    }
-
-                }
-            }
-        }
-        //если есть дубль вернет ошибку
-        if($isDubl){
-           return response(["success"=>false, "data"=>null, "message"=>"Error duble contact."],400);
-        }
+        $users = $bodyUsers->_embedded->users;
+        $randKey = array_rand($users);
+        $responsible_user = $users[$randKey];
 
         // создание контакта и сделки
-        $bodyReuqestLeadContact = array(
+        $bodyRequestLeadContact = array(
             [
                 "name"=>"Новая сделка #".$ticketNumber,
+                "created_by"=> 0,
+                "responsible_user_id" => $responsible_user->id,
                 "_embedded"=> [
                     "contacts"=> array(
                         [
@@ -134,20 +214,20 @@ Route::post('/contact', function (Request $request) {
 
         $client = new GuzzleHttp\Client([
             'verify' => false,
-
         ]);
 
         $response = $client->post(
             $url,
             [
                 'headers' => ['Authorization' => "Bearer ".$token, "Content-Type"=> "application/json"],
-                'body' => json_encode($bodyReuqestLeadContact)
+                'body' => json_encode($bodyRequestLeadContact)
             ]
         );
 
 
         $body = $response->getBody()->getContents();
         $taskTime = 4*24*60*60;
+
         // проверка на рабочее время
         if(date('w') > 1) $taskTime += 2*24*60*60;
         if((int) date('G') < 9 )
@@ -155,16 +235,19 @@ Route::post('/contact', function (Request $request) {
             $diff = 9 - (int) date('G');
             $taskTime += ($diff+2)*60*60;
         }
-        if((int) date('G') > 18 ) $taskTime -= 6*60*60;
+
+        $taskTime = time()+$taskTime;
+        $taskTime = strtotime(date('m/d/Y', $taskTime)) + 8*60*60;
 
 
         // создание задачи
         $bodyReuqestTask = array(
             [
                 "text"=>"Новая Задача #".$ticketNumber,
-                "complete_till"=> time()+$taskTime,
+                "complete_till"=> $taskTime,
                 "entity_id"=> json_decode($body)[0]->id,
-                "entity_type"=> "leads"
+                "entity_type"=> "leads",
+                "duration"=>9*60*60
             ]
         );
 
@@ -183,7 +266,7 @@ Route::post('/contact', function (Request $request) {
         );
 
         $body2 = $response2->getBody()->getContents();
-        
+
         return response(["success"=>true, "data"=> json_decode($body), "message"=>"Success ."]);
     }catch (Exception $e){
         return response(["success"=>false, "data"=>$e->getMessage(), "message"=>"Error ."], 400);
